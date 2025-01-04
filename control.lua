2a1e10a4fd10ac00de5219cf35545b85b5212a43
az4 <cagri.celebi@oredata.com>
2025-01-04 22:58:43 +0300

    Fix old entity update problem.

--- control.lua
+++ control.lua
@@ -1,125 +1,118 @@
-
 ---- Mod Settings ----
 -- local update_interval = 120
-local update_interval = settings.global["pollution_detector_update_interval"].value
-local multiplier = settings.global["pollution_detector_multiplier"].value
+local update_interval = settings.startup["pollution_detector_update_interval"].value
+local multiplier = settings.startup["pollution_detector_multiplier"].value
 local floor = math.floor
-
-script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
-  if not event then return end
-  if event.setting == "pollution_detector_update_interval" then
-    update_interval = settings.global["pollution_detector_update_interval"].value
-  end
-end)
-
-script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
-  if not event then return end
-  if event.setting == "pollution_detector_multiplier" then
-    multiplier = settings.global["pollution_detector_multiplier"].value
-  end
-end)
 
 ---- runtime Events ----
 function OnEntityCreated(event)
-  local entity
-  if event.entity and event.entity.valid then
-    entity = event.entity
-  end
-  if event.created_entity and event.created_entity.valid then
-    entity = event.created_entity
-  end
+    local entity
+    if event.entity and event.entity.valid then
+        entity = event.entity
+    end
+    if event.created_entity and event.created_entity.valid then
+        entity = event.created_entity
+    end
 
-  if entity.name == "pollution-detector" then
-    if global == nil then
-      global = {}
+    if entity.name == "pollution-detector" then
+        if storage == nil then
+            -- log("storage is nil for OnEntityCreated.")
+            storage = {}
+        end
+        if storage.Pollution_Detectors == nil then
+            -- log("storage.Pollution_Detectors is nil for OnEntityCreated.")
+            storage.Pollution_Detectors = {}
+        end
+        table.insert(storage.Pollution_Detectors, entity)
+        -- register to events after placing the first sensor
+        if #storage.Pollution_Detectors == 1 then
+            script.on_event(defines.events.on_tick, OnTick)
+            script.on_event({ defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died }, OnEntityRemoved)
+        end
     end
-    global.Pollution_Detectors = {}
-    table.insert(global.Pollution_Detectors, entity)
-    -- register to events after placing the first sensor
-    if #global.Pollution_Detectors == 1 then
-      script.on_event(defines.events.on_tick, OnTick)
-      script.on_event({defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died}, OnEntityRemoved)
-    end
-  end
 end
 
 function OnEntityRemoved(event)
-  if event.entity.name == "pollution-detector" then
+    if event.entity.name == "pollution-detector" then
 
-    for i=#global.Pollution_Detectors, 1, -1 do
-      if global.Pollution_Detectors[i].unit_number == event.entity.unit_number then
-        table.remove(global.Pollution_Detectors, i)
-      end
+        for i = #storage.Pollution_Detectors, 1, -1 do
+            if storage.Pollution_Detectors[i].unit_number == event.entity.unit_number then
+                table.remove(storage.Pollution_Detectors, i)
+            end
+        end
+
+        -- unregister when last sensor was removed
+        if #storage.Pollution_Detectors == 0 then
+            script.on_event(defines.events.on_tick, nil)
+            script.on_event({ defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died }, nil)
+        end
     end
-    
-    -- unregister when last sensor was removed
-    if #global.Pollution_Detectors == 0 then
-      script.on_event(defines.events.on_tick, nil)
-      script.on_event({defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died}, nil)
-    end
-  end
 end
 
 -- stepping from tick modulo with stride by eradicator
 function OnTick(event)
-  local offset = event.tick % update_interval
-  for i=#global.Pollution_Detectors - offset, 1, -1 * update_interval do  
-    local pollution_detector = global.Pollution_Detectors[i]    
-    local pollution_count = floor(pollution_detector.surface.get_pollution(pollution_detector.position) * multiplier)
-    local signal = { value = "pd-pollution", min = pollution_count }
-    -- Section clear code: copied from "PollutionCombinator-JamieFork".
-    for _, section in pairs(pollution_detector.get_control_behavior().sections) do
-        pollution_detector.get_control_behavior().remove_section(section.index);
+    local offset = event.tick % update_interval
+    for i = #storage.Pollution_Detectors - offset, 1, -1 * update_interval do
+        local pollution_detector = storage.Pollution_Detectors[i]
+        local pollution_count = floor(pollution_detector.surface.get_pollution(pollution_detector.position) * multiplier)
+        local signal = { value = "pd-pollution", min = pollution_count }
+        -- Section clear code: copied from "PollutionCombinator-JamieFork".
+        for _, section in pairs(pollution_detector.get_control_behavior().sections) do
+            pollution_detector.get_control_behavior().remove_section(section.index);
+        end
+        local section = pollution_detector.get_control_behavior().add_section("");
+        if (not section) then
+            return false;
+        end
+        section.set_slot(1, signal)
     end
-    local section = pollution_detector.get_control_behavior().add_section("");
-    if (not section) then
-        return false;
-    end
-    section.set_slot(1, signal)
-  end
 end
 
-
-do---- Init ----
+---- Init ----
 local function init_Pollution_Detectors()
-  -- gather all pollution detectors on every surface in case another mod added some
-  if global == nil then
-    global = {}
-  end
-  global.Pollution_Detectors = {}
-  for _, surface in pairs(game.surfaces) do
-    pollution_detectors = surface.find_entities_filtered {
-      name = "pollution-detector",
-    }
-    for _, pollution_detector in pairs(pollution_detectors) do
-      table.insert(global.Pollution_Detectors, pollution_detector)
+    -- gather all pollution detectors on every surface in case another mod added some
+
+    if storage == nil then
+        -- log("storage is nil for init_Pollution_Detectors.")
+        storage = {}
     end
-  end
+    if storage.Pollution_Detectors == nil then
+        -- log("storage.Pollution_Detectors is nil for init_Pollution_Detectors.")
+        storage.Pollution_Detectors = {}
+    end
+    for _, surface in pairs(game.surfaces) do
+        -- log("surface.name: " .. surface.name)
+        pollution_detectors = surface.find_entities_filtered {
+            name = "pollution-detector",
+        }
+        for _, pollution_detector in pairs(pollution_detectors) do
+            table.insert(storage.Pollution_Detectors, pollution_detector)
+        end
+    end
 end
 
 local function init_events()
-    script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity, defines.events.script_raised_built, defines.events.script_raised_revive}, OnEntityCreated)
-    if global == nil then
-      global = {}
-    end
-    if global.Pollution_Detectors and next(global.Pollution_Detectors) then
+    script.on_event({ defines.events.on_built_entity, defines.events.on_robot_built_entity, defines.events.script_raised_built, defines.events.script_raised_revive }, OnEntityCreated)
+
+    if storage and storage.Pollution_Detectors and next(storage.Pollution_Detectors) then
         script.on_event(defines.events.on_tick, OnTick)
-        script.on_event({defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died}, OnEntityRemoved)
+        script.on_event({ defines.events.on_pre_player_mined_item, defines.events.on_robot_pre_mined, defines.events.on_entity_died }, OnEntityRemoved)
     end
 end
 
 script.on_load(function()
-  init_events()
+    -- log("on_load called.")
+    init_events()
 end)
 
 script.on_init(function()
-  init_Pollution_Detectors()
-  init_events()
+    -- log("on_init called.")
+    init_Pollution_Detectors()
+    init_events()
 end)
 
 script.on_configuration_changed(function(data)
-  init_Pollution_Detectors()
-  init_events()
+    -- log("on_configuration_changed called.")
+    init_Pollution_Detectors()
+    init_events()
 end)
-
-end
--- settings.lua
+++ settings.lua
@@ -2,8 +2,8 @@ data:extend({
   {  
     type = "int-setting",
     name = "pollution_detector_update_interval",
-    order = "aa",
-    setting_type = "runtime-global",
+    order = "aa1",
+    setting_type = "startup",
     default_value = 120,
     minimum_value = 1,
     maximum_value = 216000, -- 1h
@@ -11,8 +11,8 @@ data:extend({
   {  
     type = "int-setting",
     name = "pollution_detector_multiplier",
-    order = "aa",
-    setting_type = "runtime-global",
+    order = "aa2",
+    setting_type = "startup",
     default_value = 60,
     minimum_value = 1,
     maximum_value = 60000000,
